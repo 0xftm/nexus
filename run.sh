46c26db2cf4a3af7f78ee54d35a29b14b16643a6
FLYTOMOON <115777868+0xftm@users.noreply.github.com>
2024-12-11 13:53:13 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+clear
+
+echo "===========NEXUS======= " && sleep 1
+
+sudo tee /etc/systemd/system/nexus.service > /dev/null <<EOF
+[Unit]
+Description=NEXUS
+After=network-online.target
+[Service]
+User=root
+WorkingDirectory=/root
+ExecStart=/bin/bash -c 'curl https://cli.nexus.xyz | sh'
+Restart=always
+StandardOutput=append:/root/nexus-ln.log
+StandardError=append:/root/nexus-ln.log
+RestartSec=3
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable nexus
+sudo systemctl restart nexus
